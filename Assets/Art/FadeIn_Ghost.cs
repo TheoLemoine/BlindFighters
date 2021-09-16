90eb0a979c43189249df15669b1c4fd1047930c2
Miguel <saura1993@gmail.com>
2021-09-16 16:38:33 +0200

    Ghosts, bats and lights

---
+++ Assets/Art/FadeIn_Ghost.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn_Ghost : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
