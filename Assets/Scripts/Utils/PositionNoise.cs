d6e52eb586eda5fbc2aa47d0182e6431a24fb36e
Theo Lemoine <theo.lemoine0@gmail.com>
2021-09-16 12:05:10 +0200

    Integrated bats and ghost

---
+++ Assets/Scripts/Utils/PositionNoise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Utils
+{
+    public class PositionNoise : MonoBehaviour
+    {
+        [SerializeField] private float force;
+        
+        private void FixedUpdate()
+        {
+            transform.localPosition += new Vector3(Random.Range(-force, force), Random.Range(-force, force), Random.Range(-force, force)) * Time.fixedDeltaTime;
+        }
+    }
+}
